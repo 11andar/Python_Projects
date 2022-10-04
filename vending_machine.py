eda119cd394013be9d2f3d6d2cf40fe2ce941c53
Jedrek Darowski <11andar07@gmail.com>
2022-10-04 14:24:57 +0200

    1. TODO list

---
+++ vending_machine.py
@@ -0,0 +1,15 @@
+# Show a list of availible products and their prices
+
+# Let the user choose a product
+
+# Create vending machine mechanism for each product
+    
+# Each time user inserts a coin print amount due left and prompt for coin again
+
+# Print change owed
+
+# Ask if user wants to buy more products
+
+# If yes: repeat all previous steps
+
+# Else: end
